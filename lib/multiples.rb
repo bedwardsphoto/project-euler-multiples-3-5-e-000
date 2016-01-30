d3df23e613b4e64cc987e3c416012698dd754f01
bedwardsphoto <brian@brianedwardsphotography.com>
2016-01-29 21:17:05 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
-# Enter your procedural solution here!
+
+def collect_multiples(limit)
+  collection=Array.new
+num=1
+until num==limit
+    collection<<num
+  num+=1
+end
+collection.select {|i| (i%3==0 || i%5==0)}
+end
+
+def sum_multiples(limit)
+  sum=0
+  collect_multiples(limit).each {|a| sum+=a}
+  return sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,27 @@
-# Enter your object-oriented solution here!
+class Multiples
+def  initialize (limit)
+  @limit=limit
+end
+def limit=(limit)
+end
+def limit
+  @limit
+end
+
+def collect_multiples
+  collection=Array.new
+num=1
+until num==@limit
+    collection<<num
+  num+=1
+end
+collection.select {|i| (i%3==0 || i%5==0)}
+end
+
+def sum_multiples
+  sum=0
+  collect_multiples.each {|a| sum+=a}
+  return sum
+end
+
+end
